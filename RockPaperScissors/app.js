66e58a3b6951a0e721c69a578e9668cead579c1d
Colin Pinegar <colinpin@gmail.com>
2016-01-25 16:49:22 -0700

    comments

--- RockPaperScissors/app.js
+++ RockPaperScissors/app.js
@@ -1,14 +1,14 @@
-/* KEY
-— — — — — — —
-ROCK = 1
-PAPER = 2
-SCISSORS = 3  */
+// THIS IS WHAT THE ROBOT PLAYS:
+// 1 = ROCK
+// 2 = PAPER
+// 3 = SCISSORS
 
 var play = function(item){
-    var player = item;
-    var opponent = Math.floor(Math.random() * 3) + 1;
-    console.log(player);
-    console.log(opponent);
+    var player = item; //Player plays what's on the button
+    var opponent = Math.floor(Math.random() * 3) + 1; //This gets a random number between 1 and 3
+    console.log(player); //Logs the player's choice
+    console.log(opponent); //Logs the computer's choice (number)
+    //Now the Logic behind who wins and who loses
     if (((player === "rock") && (opponent === 1)) || ((player === "paper") && (opponent === 2)) || ((player === "scissors") && (opponent === 3))) {
         document.getElementById("result").textContent = "It's a Tie!";
     } else if (((player === "rock") && (opponent === 3)) || ((player === "paper") && (opponent === 1)) || ((player === "scissors") && (opponent === 2))) {
